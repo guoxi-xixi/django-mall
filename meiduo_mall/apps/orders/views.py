6e3f1bffe51fe37ae6679c6d7dd71e2920792631
xixi <446837204@qq.com>
2022-05-29 02:22:03 +0800

    乐观锁解决并发下单问题
    Signed-off-by: xixi <446837204@qq.com>

--- meiduo_mall/apps/orders/views.py
+++ meiduo_mall/apps/orders/views.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 from django.shortcuts import render
 
@@ -277,32 +278,56 @@ class OrderCommitView(LoginRequiredJSONMixin, View):
             #
             #       2.5 遍历 根据选中商品的id进行查询
             for sku_id, count in carts.items():
-                try:
-                    sku = SKU.objects.get(id=sku_id)
-                except Exception as e:
-                    logger.error(e)
-                    return JsonResponse({'code': 400, 'errmsg': '获取商品信息失败'})
-                else:
-                    # 2.6 判断库存是否充足，
-                    if sku.stock < count:
-                        # 2.7 如果不充足，下单失败
-                        transaction.savepoint_rollback(point) # 库存不足，回滚事务
-                        return JsonResponse({'code': 400, 'errmsg': '库存不足'})
+
+                while True:
+                    try:
+                        sku = SKU.objects.get(id=sku_id)
+                    except Exception as e:
+                        logger.error(e)
+                        return JsonResponse({'code': 400, 'errmsg': '获取商品信息失败'})
                     else:
-                        # 2.8 如果充足，则库存减少，销量增加
-                        sku.stock -= count
-                        sku.sales += count
-                        sku.save()
-                        # 2.9 累加总数量和总金额
-                        order_info.total_amount += (sku.price*count)
-                        order_info.total_count += count
-                        # 2.10 保存订单商品信息
-                        OrderGoods.objects.create(
-                            order=order_info,
-                            sku=sku,
-                            count=count,
-                            price=sku.price
-                        )
+                        # 2.6 判断库存是否充足，
+                        if sku.stock < count:
+                            # 2.7 如果不充足，下单失败
+                            transaction.savepoint_rollback(point) # 库存不足，回滚事务
+                            return JsonResponse({'code': 400, 'errmsg': '库存不足'})
+                        else:
+                            # 2.8 如果充足，则库存减少，销量增加
+                            # sku.stock -= count
+                            # sku.sales += count
+                            # sku.save()
+                            # 解决并发问题：超卖bug： -- 加乐观锁
+
+                            # 2.8.1. 先记录某一个数据    记录哪个数据都可以
+                            old_stock = sku.stock   # #旧库存 参照这个记录
+                            # 2.8.2 更新的时候，再比对一下这个记录对不对
+                            new_stock = sku.stock - count
+                            new_sales = sku.sales + count
+
+                            # 判断 stock=old_stock 为true，说明没有人更新数据，此时执行更新数据，否则不更新数据，加乐观锁
+                            result = SKU.objects.filter(id=sku_id, stock=old_stock).update(stock=new_stock, sales=new_sales)
+                            # result = 1 表示 有1条记录修改成功
+                            # result = 0 表示 没有更新    -> 说明此时有人已更新了数据，重新执行
+                            if result == 0:
+                                # time.sleep(5)
+                                # transaction.savepoint_rollback(point)   # 更新失败，回滚数据
+                                # return JsonResponse({'code': 400, 'errmsg': '下单失败～～～～～'})
+
+                                # 如果下单失败，但是库存足够时，继续下单，直到下单成功或者库存不足为止
+                                time.sleep(0.005)
+                                continue
+
+
+                            # 2.9 累加总数量和总金额
+                            order_info.total_amount += (sku.price*count)
+                            order_info.total_count += count
+                            # 2.10 保存订单商品信息
+                            OrderGoods.objects.create(
+                                order=order_info,
+                                sku=sku,
+                                count=count,
+                                price=sku.price
+                            )
 
             #   3.更新订单的总金额和总数量
             order_info.total_amount += freight  # 添加运费
@@ -316,3 +341,62 @@ class OrderCommitView(LoginRequiredJSONMixin, View):
         # pipeline.execute()
         # 四。返回响应
         return JsonResponse({'code': 0, 'msg': 'ok', 'order_id': order_id})
+
+
+"""
+解决并发的超卖问题：
+① 队列
+② 锁
+    悲观锁： 当查询某条记录时，即让数据库为该记录加锁，锁住记录后别人无法操作
+
+            悲观锁类似于我们在多线程资源竞争时添加的互斥锁，容易出现死锁现象
+
+    举例：
+
+    甲   1,3,5,7
+
+    乙   2,4,7,5
+
+
+    乐观锁:    乐观锁并不是真的锁。
+            在更新的时候判断此时的库存是否是之前查询出的库存，
+            如果相同，表示没人修改，可以更新库存，否则表示别人抢过资源，不再执行库存更新。
+
+
+
+    举例：
+                桌子上有10个肉包子。  9    8 
+
+                现在有5个人。 这5个人，每跑1km。只有第一名才有资格吃一个肉包子。 
+
+                5
+
+                4
+
+                3 
+
+    步骤：
+            1. 先记录某一个数据  
+            2. 我更新的时候，再比对一下这个记录对不对  
+
+
+MySQL数据库事务隔离级别主要有四种：
+
+    Serializable：串行化，一个事务一个事务的执行。  用的并不多
+
+    Repeatable read：可重复读，无论其他事务是否修改并提交了数据，在这个事务中看到的数据值始终不受其他事务影响。
+
+v    Read committed：读取已提交，其他事务提交了对数据的修改后，本事务就能读取到修改后的数据值。
+
+    Read uncommitted：读取未提交，其他事务只要修改了数据，即使未提交，本事务也能看到修改后的数据值。
+
+
+    举例：     5,7 库存 都是  8
+
+    甲   5,   7        5
+
+    乙  7,    5         5
+
+
+MySQL数据库默认使用可重复读（Repeatable read）
+"""
