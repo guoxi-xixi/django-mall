f06ce6cc0a73e5eb90090c22ec431da80ee1e592
xixi <446837204@qq.com>
2022-06-12 16:07:02 +0800

    meiduo_amdin后台 只允许is_staff：true用户登录
    Signed-off-by: xixi <446837204@qq.com>

--- meiduo_mall/apps/meiduo_admin/urls.py
+++ meiduo_mall/apps/meiduo_admin/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from rest_framework_jwt.views import obtain_jwt_token
+# from rest_framework_jwt.views import obtain_jwt_token
+from apps.meiduo_admin.user import meiduo_token
 
 urlpatterns = {
     # 项目根urls 中配置 根路由可省略meiduo_admin
     # path('meiduo_admin/authorizations/', obtain_jwt_token),
-    path('authorizations/', obtain_jwt_token),
+    # path('authorizations/', obtain_jwt_token),    # 系统默认视图
+    path('authorizations/', meiduo_token),  # 重写后 认证用户的视图
 }
