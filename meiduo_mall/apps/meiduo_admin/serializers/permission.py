ce6eb47e84b60f4c4488d1ee6b0caf97a77c9621
xixi <446837204@qq.com>
2022-06-27 01:09:52 +0800

    meiduo_amdin后台 Permission - 管理员管理 - 新增和更新
    Signed-off-by: xixi <446837204@qq.com>

--- meiduo_mall/apps/meiduo_admin/serializers/permission.py
+++ meiduo_mall/apps/meiduo_admin/serializers/permission.py
@@ -41,3 +41,33 @@ class AdminUserModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+        extra_kwargs = {
+            'password': {
+                'write_only': True
+            },
+        }
+
+    def create(self, validated_data):
+        # 调用父类的create
+        # user = super(AdminUserModelSerializer, self).create(validated_data)
+        user = super().create(validated_data)
+
+        # 密码加密
+        user.set_password(validated_data.get('password'))
+        # 后台用户
+        user.is_staff = True
+        user.save()
+
+        return user
+
+    def update(self, instance, validated_data):
+        # 调用父类的update
+        super(AdminUserModelSerializer, self).update(instance, validated_data)
+
+        # 密码加密
+        password = validated_data.get('password')
+        if password:
+            instance.set_password(password)
+            instance.save()
+
+        return instance
